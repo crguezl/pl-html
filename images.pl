b051578798fde109d8c63eacd9d52fc9db3898b4
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-04-28 07:47:27 +0100

    m

--- images.pl
+++ images.pl
@@ -32,6 +32,13 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img207.png"
  ALT="$ \vert x\vert$">|; 
 
+$key = q/%latex2htmlidmarker43913S=program;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img108.png"
+ ALT="% latex2html id marker 43913
+$ S = program$">|; 
+
 $key = q/existsS{array}{c}*Longrightarrow{scriptstyleRM}{array}betaAbx{array}{c}*Longrightarrow{scriptstyleRM}{array}betaalphabx=gamma;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="228" HEIGHT="82" ALIGN="MIDDLE" BORDER="0"
@@ -763,11 +770,11 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img176.png"
  ALT="$ \overline{A}$">|; 
 
-$key = q/Sstackrel{*}{Longrightarrow}yAzLongrightarrowywz=x;MSF=1.6;LFS=11;AAT/;
+$key = q/(x,y);MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="187" HEIGHT="45" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img111.png"
- ALT="$ S \stackrel{*}{\Longrightarrow} y A z \Longrightarrow y w z = x$">|; 
+ WIDTH="45" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img361.png"
+ ALT="$ (x, y)$">|; 
 
 $key = q/ASSIGN(NUM,PRINT(ID));MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -775,17 +782,17 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img247.png"
  ALT="$ ASSIGN(NUM, PRINT(ID))$">|; 
 
-$key = q/(x,y);MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="45" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img361.png"
- ALT="$ (x, y)$">|; 
-
 $key = q/Longleftarrow;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="33" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img220.png"
  ALT="$ \Longleftarrow$">|; 
+
+$key = q/Sstackrel{*}{Longrightarrow}yAzLongrightarrowywz=x;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="187" HEIGHT="45" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img111.png"
+ ALT="$ S \stackrel{*}{\Longrightarrow} y A z \Longrightarrow y w z = x$">|; 
 
 $key = q/displaystylemientras(epsiloninFIRST(Y_i)and(ileqk));MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -2378,13 +2385,6 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="18" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img183.png"
  ALT="$ I_i$">|; 
-
-$key = q/%latex2htmlidmarker43911S=program;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img108.png"
- ALT="% latex2html id marker 43911
-$ S = program$">|; 
 
 $key = q/Arightarrowgamma_1midldotsmidgamma_n;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
