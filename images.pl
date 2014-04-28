126c9877db70ff5f5243c14e856665a6a32ab429
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-04-28 16:36:29 +0100

    m

--- images.pl
+++ images.pl
@@ -763,18 +763,6 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img176.png"
  ALT="$ \overline{A}$">|; 
 
-$key = q/Sstackrel{*}{Longrightarrow}yAzLongrightarrowywz=x;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="187" HEIGHT="45" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img111.png"
- ALT="$ S \stackrel{*}{\Longrightarrow} y A z \Longrightarrow y w z = x$">|; 
-
-$key = q/ASSIGN(NUM,PRINT(ID));MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="251" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img247.png"
- ALT="$ ASSIGN(NUM, PRINT(ID))$">|; 
-
 $key = q/(x,y);MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="45" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
@@ -786,6 +774,18 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="33" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img220.png"
  ALT="$ \Longleftarrow$">|; 
+
+$key = q/Sstackrel{*}{Longrightarrow}yAzLongrightarrowywz=x;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="187" HEIGHT="45" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img111.png"
+ ALT="$ S \stackrel{*}{\Longrightarrow} y A z \Longrightarrow y w z = x$">|; 
+
+$key = q/ASSIGN(NUM,PRINT(ID));MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="251" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img247.png"
+ ALT="$ ASSIGN(NUM, PRINT(ID))$">|; 
 
 $key = q/displaystylemientras(epsiloninFIRST(Y_i)and(ileqk));MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -1695,13 +1695,6 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img244.png"
  ALT="$ \rho(ID) = \rho(NUM) = \rho(LEFTVALUE) = \rho(STR) = 0$">|; 
 
-$key = q/%latex2htmlidmarker43964S=program;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img108.png"
- ALT="% latex2html id marker 43964
-$ S = program$">|; 
-
 $key = q/ainSigma;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="48" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
@@ -2045,6 +2038,13 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="543" HEIGHT="186" BORDER="0"
  SRC="|."$dir".q|img141.png"
  ALT="\begin{figure}\centerline{\epsfig{file=chapter_bottomup/nfa.eps, width=12cm}}\end{figure}">|; 
+
+$key = q/%latex2htmlidmarker44012S=program;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img108.png"
+ ALT="% latex2html id marker 44012
+$ S = program$">|; 
 
 $key = q/rho(PLUS)=rho(TIMES)=rho(ASSIGN)=2;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
