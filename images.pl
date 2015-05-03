ed666f148cc5257936b5017035bdedbfc16ea07f
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-05-03 21:37:40 +0100

    m

--- images.pl
+++ images.pl
@@ -378,12 +378,6 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img49.png"
  ALT="$ FIRST(\alpha)$">|; 
 
-$key = q/tslash2ldotp1;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="45" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img306.png"
- ALT="$ t/2\ldotp1$">|; 
-
 $key = q/FSD;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="45" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
@@ -395,6 +389,12 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="232" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img353.png"
  ALT="$ ASSIGN(x, TIMES(y,ID))$">|; 
+
+$key = q/tslash2ldotp1;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="45" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img306.png"
+ ALT="$ t/2\ldotp1$">|; 
 
 $key = q/BrightarrowY_1ldotsY_n;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
