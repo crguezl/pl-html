1ae7e97a66ff4b6c0083ee70d8a67be86577486f
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-05-10 18:22:38 +0100

    m

--- images.pl
+++ images.pl
@@ -572,6 +572,13 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img198.png"
  ALT="$ error$">|; 
 
+$key = q/%latex2htmlidmarker21974S=program;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img100.png"
+ ALT="% latex2html id marker 21974
+$ S = program$">|; 
+
 $key = q/tldotp1{xslashA};MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="76" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
@@ -2014,13 +2021,6 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="291" HEIGHT="82" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img102.png"
  ALT="$ FSD = \left \{ \alpha \in (\Sigma \cup V)* : \exists S \begin{array}{c} * \Longrightarrow  {\scriptstyle RM} \end{array} \alpha \right \}$">|; 
-
-$key = q/%latex2htmlidmarker21963S=program;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="107" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img100.png"
- ALT="% latex2html id marker 21963
-$ S = program$">|; 
 
 $key = q/PLUS(NUM_1,NUM_2)LongrightarrowNUM_3;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
